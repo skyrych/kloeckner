b10c6e333dd2e186224da080b3404546ae7657f4
Serhii Kyrychenko <skir79@gmail.com>
2021-08-30 15:28:18 +0300

    My Python thing

---
+++ pars.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.9
+
+import sys
+import os
+
+path = sys.argv[1]
+file_name = path.split("=")
+
+read = open(file_name[1])
+lines = read.readlines()
+output = open("parsed_log.log", "w")
+
+for line in lines:
+    lst=(line.split(" "))
+    print('{' + '\n' + '\t' + '"@timestamp": ' + lst[0] + ',' + '\n' + '\t' + '"stream": ' + lst[1] + ',' + '\n' + '\t' + '"log": ' + ' '.join(map(str, lst[16:-1])) + '\n' + '}', file = output)
+
+read.close()
+output.close()
